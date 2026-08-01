be1d84ab89207c6fba6142e53aa4f5aca527f1e3
manvir bhangu <manvirbhangu222@gmail.com>
2026-08-01 10:40:29 +0530

    Create max_avg_subarray.py

---
+++ ARRAYS/max_avg_subarray.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        window_sum=sum(nums[:k])
+        max_sum=window_sum
+        for i in range(k,len(nums)):
+            window_sum-=nums[i-k]
+            window_sum+=nums[i]
+            max_sum=max(max_sum,window_sum)
+        return max_sum/float(k)
