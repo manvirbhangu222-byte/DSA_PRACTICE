0e39699304f8394d4b4b5c46d934d05ee58563a2
manvir bhangu <manvirbhangu222@gmail.com>
2026-08-18 14:28:22 +0530

    solved linked list medium problems

---
+++ LINKED LIST/remove_nth_node_from_end.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+    
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        slow = dummy
+        fast = dummy
+
+        for _ in range(n):
+            fast = fast.next
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
+       
+        
+        
