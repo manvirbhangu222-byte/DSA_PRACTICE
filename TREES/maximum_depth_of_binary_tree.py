11a75734f20a8c39e9d92f5740d2b69694c4a131
manvir bhangu <manvirbhangu222@gmail.com>
2026-08-23 12:05:17 +0530

    Create maximum_depth_of_binary_tree.py

---
+++ TREES/maximum_depth_of_binary_tree.py
@@ -0,0 +1,14 @@
+class Treenode(object):
+    def __init__(self,val=0,left=None,right=None):
+        self.val=val
+        self.right=right
+        self.left=left
+        
+class Solution(object):
+    def maxDepth(self,root):
+        if root is None:
+            return None
+        return 1 + max(
+            self.maxDepth(root.left),
+            self.maxDepth(root.right)
+        )
