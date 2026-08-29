63eadc3dc1e044bdd19b56a6836154094f041da1
manvir bhangu <manvirbhangu222@gmail.com>
2026-08-29 18:34:04 +0530

    Create course_schedule.py

---
+++ EXTRA/course_schedule.py
@@ -0,0 +1,31 @@
+
+class Solution:
+    def canFinish(self, numCourses, prerequisites):
+
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for course, prerequisite in prerequisites:
+            graph[prerequisite].append(course)
+            indegree[course] += 1
+
+        queue = deque()
+
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+
+        taken = 0
+
+        while queue:
+            course = queue.popleft()
+            taken += 1
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        return taken == numCourses
+        
